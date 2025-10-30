2332cedee3a4e710acee9015f266474d349eafeb
ClaheEventos <personaldeit@claheeventos.com>
2025-10-30 10:36:50 -0300

    Mi cambio local en settings antes de pull

--- cocinita/settings.py
+++ cocinita/settings.py
@@ -50,7 +50,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 CORS_ALLOWED_ORIGINS = [
+    "https://48p82xms-8002.brs.devtunnels.ms",
     "http://localhost:5173",
+    'http://127.0.0.1:8002',
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
